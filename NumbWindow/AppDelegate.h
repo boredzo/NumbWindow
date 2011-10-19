3510fd11ac9b783afd6a135ee762b7a709442622
Peter Hosey <prh@Featherweight.local>
2011-10-19 01:09:16 -0700

    Initial Commit

---
+++ NumbWindow/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NumbWindow
+//
+//  Created by Peter Hosey on 2011-10-19.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+@property (assign) IBOutlet NSWindow *window;
+
+@end
---
+++ NumbWindow/AppDelegate.m
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.m
+//  NumbWindow
+//
+//  Created by Peter Hosey on 2011-10-19.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+@synthesize window = _window;
+
+- (void)dealloc
+{
+    [super dealloc];
+}
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
